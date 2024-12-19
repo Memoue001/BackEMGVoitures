ea836952e9578d350167d49dab2cedf08328b8b0
MEMOUE <konememoue1998@gmail.com>
2024-12-19 09:00:00 +0000

    commit 11

--- Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace EMGVoitures.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-        // Vous pouvez ajouter des propriétés supplémentaires si nécessaire
-    }
-}
